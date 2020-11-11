51b9c719df231f5cd66858abdbbd1b4b11f6db0c
Nik Sofianos <nsofianos@hotmail.com>
2020-11-11 23:26:00 +0000

    add registration page

--- express_server.js
+++ express_server.js
@@ -33,6 +33,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/register", (req, res) => {
+  const templateVars = { username: req.cookies["username"]};
+  res.render("urls_register", templateVars);
+});
+
 app.get("/urls/new", (req, res) => {
   const templateVars = { username: req.cookies["username"] };
   res.render("urls_new", templateVars);
