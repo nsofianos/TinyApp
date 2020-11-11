d0a09a1a6e0a7177d55847aed78c52ffa5a492dd
Nik Sofianos <nsofianos@hotmail.com>
2020-11-11 00:12:50 +0000

    add option to edit longurls

--- express_server.js
+++ express_server.js
@@ -67,6 +67,12 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect('/urls');
 });
 
+app.post("/urls/:shortURL/edit", (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.longURL;
+  res.redirect('/urls');
+});
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
